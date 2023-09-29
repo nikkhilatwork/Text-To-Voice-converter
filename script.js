e11ecdaefc8b88fbbd86e1692845df3113fb7926
Nikhil uttam zolekar <121855218+nikkhilatwork@users.noreply.github.com>
2023-09-29 21:06:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let speech  =  new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelecet  = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelecet.options[i] = new Option(voice.name, i)));
+
+}
+
+voiceSelecet.addEventListener("change", () =>{
+    speech.voice = voices[voiceSelecet.value];
+});
+
+
+
+
+
+
+document.querySelector("button").addEventListener("click", () =>{
+speech.text = document.querySelector("textarea").value;
+window.speechSynthesis.speak(speech);    
+
+});
